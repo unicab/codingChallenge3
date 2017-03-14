2da0291996b1c2fee6ada4dd3fb81cc0b3b632e1
unicab <tamnhinho1989@gmail.com>
2017-03-14 16:30:25 -0600

    added comments and fixed a few bugs

---
+++ googlePlaceSearch/Utilities/UINavigationController+Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  UINavigationController+Extensions.swift
+//  googlePlaceSearch
+//
+//  Created by remotetiger on 3/14/17.
+//  Copyright © 2017 TamNguyen. All rights reserved.
+//
+
+import Foundation
